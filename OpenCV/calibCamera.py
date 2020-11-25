52033b093d31eed604b756bb489c6d6e535bfba8
Ioannis Broumas <giannibrouma@gmail.com>
2020-11-26 00:55:43 +0200

    added camera calibration

---
+++ OpenCV/calibCamera.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python3
+
+# Ioannis Broumas
+# ioabro17@student.hh.se
+# Nov 2020
+# Remember OpenCV is BGR
+
+import os
+import numpy as np
+import cv2 as cv
+import glob
+import pickle
+
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)*23
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+cam = cv.VideoCapture(0)
+
+count = 0
+
+while count < 21:
+
+    # Capturing each frame of our video stream
+    ret, QueryImg = cam.read()
+    gray = cv.cvtColor(QueryImg, cv.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (9,6), None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners)
+        count += 1
+        # Draw and display the corners
+        cv.drawChessboardCorners(QueryImg, (9,6), corners2, ret)
+        cv.imshow('img', QueryImg)
+        cv.waitKey(500)
+
+cv.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+# Print matrix and distortion coefficient to the console
+print(mtx)
+print(dist)
+
+# Output values to be used where matrix+dist is required
+f = open('calibration.pckl', 'wb')
+pickle.dump((mtx, dist), f)
+f.close()
+
+print(tvecs)
