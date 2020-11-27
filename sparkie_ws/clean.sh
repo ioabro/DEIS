9b0ab5706feab03da639fb7527e1d5bbb8c34d9f
Ioannis Broumas <giannibrouma@gmail.com>
2020-11-27 23:24:12 +0200

    added clean.sh

---
+++ sparkie_ws/clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# run this script when you think that the changes in the files
+# are not reflected in the executables after colcon build
+
+# Christo George
+# christogeorge@live.in
+
+rm -r build/deis_py_dev/
+rm -r install/deis_py_dev/
