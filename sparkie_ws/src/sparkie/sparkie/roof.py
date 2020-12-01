07254bcdf9a9604b8d71da4d00f47205c380e34b
Ioannis Broumas <giannibrouma@gmail.com>
2020-12-01 19:48:22 +0200

    added rooftop camera

--- sparkie_ws/src/sparkie/sparkie/roof.py
+++ sparkie_ws/src/sparkie/sparkie/roof.py
@@ -4,6 +4,8 @@
 # ioabro17@student.hh.se
 # Dec 2020
 # Remember OpenCV is BGR
+# Read the camera on the roof and detect the location of ArUco markers on the map
+# Can be adapted to detect something else on the board
 
 import numpy as np
 import cv2 as cv
@@ -13,7 +15,7 @@ from cv2 import aruco
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-from geometry_msgs.msg import PointStamped
+from geometry_msgs.msg import Point
 
 Sparkie_ID = 1 # Change accordingly
 Drone_ID = 2 # Change accordingly
@@ -26,31 +28,34 @@ class Roof(Node):
 
     def __init__(self):
         super().__init__('roof')
-        self.sparkie_publisher_ = self.create_publisher(PointStamped, 'sparkieGPS', 10)
-        self.drone_publisher_ = self.create_publisher(PointStamped, 'droneGPS', 10)
+        self.sparkie_publisher_ = self.create_publisher(Point, 'sparkieGPS', 10)
+        self.drone_publisher_ = self.create_publisher(Point, 'droneGPS', 10)
         self.get_logger().info("Node Roof initialized!")
 
 def main(args=None):
     rclpy.init(args=args)
 
     r = Roof()
-    point = PointStamped()
+    point = Point()
 
     # IP camera on the ceiling
-    cam = cv.VideoCapture('rtsp://192.168.1.2:554/axis.media/media.amp')
+    cam = cv.VideoCapture('rtsp://192.168.1.4:554/axis-media/media.amp')
 
     if not cam.isOpened():
         print('--(!)Error opening video capture')
+        cam.release()
+        cv.destroyAllWindows()
         r.destroy_node()
         rclpy.shutdown()
+        exit()
 
     HorizontalPixels = cam.get(3)
     # print(cam.get(3)) # Width - Horizontal
     VerticalPixels = cam.get(4)
     # print(cam.get(4)) # Height - Vertical
     # Resolution = HorizontalPixels * VerticalPixels
-    VerticalDistance = 2425
-    HorizontalDistance = 3635
+    VerticalDistance = 2225 # 2425
+    HorizontalDistance = 3035 # 3635
     # Calculate mm to pixels
     y_mm_to_pixel = VerticalDistance / VerticalPixels
     x_mm_to_pixel = HorizontalDistance / HorizontalPixels
@@ -79,14 +84,13 @@ def main(args=None):
                         X = x_mm_to_pixel * cx
                         Y = y_mm_to_pixel * cy
                         # Prepare and publish the point
-                        point.header.frame_id = "sparkieGPS"
-                        point.header.stamp = r.get_clock().now().to_msg()
                         point.x = X
                         point.y = Y
-                        point.z = 0
-                        r.sparkie_publisher_(point)
+                        point.z = .0
+                        r.sparkie_publisher_.publish(point)
                         # Outline the detected marker in our image
-                        frame = aruco.drawDetectedMarkers(frame, corner, borderColor=(0, 0, 255))
+                        # frame = aruco.drawDetectedMarkers(frame, corner, borderColor=(0, 0, 255))
+                        print(X, Y)
                     if i == Drone_ID:
                         # Find X, Y of the center pixel
                         cx = (corner[0][0][0] + corner[0][1][0] + corner[0][2][0] + corner[0][3][0]) / 4
@@ -95,18 +99,19 @@ def main(args=None):
                         X = x_mm_to_pixel * cx
                         Y = y_mm_to_pixel * cy
                         # Prepare and publish the point
-                        point.header.frame_id = "droneGPS"
-                        point.header.stamp = r.get_clock().now().to_msg()
                         point.x = X
                         point.y = Y
-                        point.z = 0
-                        r.drone_publisher_(point)
+                        point.z = .0
+                        r.drone_publisher_.publish(point)
                         # Outline the detected marker in our image
-                        frame = aruco.drawDetectedMarkers(frame, corner, borderColor=(0, 0, 255))
+                        # frame = aruco.drawDetectedMarkers(frame, corner, borderColor=(0, 0, 255))
+                        print(X, Y)
+                        
+            frame = aruco.drawDetectedMarkers(frame, corners, borderColor=(0, 0, 255))
             # Show the frame
             cv.imshow('A-GPS', frame)
 
-            if cv.waitKey(100) == 27: # Wait for 10ms, if key == 27 (esc char) break
+            if cv.waitKey(10) == 27: # Wait for 10ms, if key == 27 (esc char) break
                 break
                 
     cam.release()
