a6dd1ff27aadff7206ff9beca758360a7abc68ac
Ioannis Broumas <giannibrouma@gmail.com>
2020-12-15 00:36:05 +0200

    corrected gps node

--- sparkie_ws/src/sparkie/sparkie/gps.py
+++ sparkie_ws/src/sparkie/sparkie/gps.py
@@ -9,13 +9,13 @@ from math import floor
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-from geometry_msgs.msg import PointStamped
+from geometry_msgs.msg import Point
 
 class GPS(Node):
 
     def __init__(self):
         super().__init__('gps')
-        self.publisher_ = self.create_publisher(PointStamped, 'GPS', 10)
+        self.publisher_ = self.create_publisher(Point, 'GPS', 10)
         self.subscription = self.create_subscription(
             String,
             '/robotPositions',
@@ -28,19 +28,7 @@ class GPS(Node):
         data = msg.data.strip("[']")
         data = data.split(sep=";")
         data = data[0] # Group1 should be first, change accordingly
-        Latitude = float(data[0])
-        Longitude = float(data[1])
-        LongDeg = floor(Longitude/100) + (Longitude - floor(Longitude/100)*100)/60
-        LatDeg = floor(Latitude/100) + (Latitude - floor(Latitude/100)*100)/60
-
-        F_lon = 62393
-        F_lat = 111342
-
-        X = F_lon * LongDeg
-        Y = F_lat * LatDeg
-        point = PointStamped()
-        point.header.frame_id = "gps"
-        point.header.stamp = timestamp
+        point = Point()
         point.x = X
         point.y = Y
         point.z = 0
